8540ffdefd4cc2df695c0f2d1a0cf4f717dd2b80
deok2 <dukgi77@naver.com>
2022-02-07 14:13:41 +0900

    initial settings with account, articles

---
+++ backend/articles/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Article
+
+from accounts.serializers import UserSerializer
+
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'created_at']
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Article
+        fields = '__all__'
