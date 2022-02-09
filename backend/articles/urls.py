8b096400e358dae7a1b5271f1ba3d88368a85bb4
deok2kim <deok2kim>
2022-02-09 21:00:53 +0900

    add articles url, requirements.txt

---
+++ backend/articles/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'articles'
+urlpatterns = [
+    path('', views.article_list),
+    path('create/', views.create_article),
+    path('<int:article_pk>', views.article_detail),
+]
