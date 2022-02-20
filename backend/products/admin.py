300f232253b6c0119493fc5daa7cd07bb0cbec8d
deok2kim <deok2kim>
2022-02-20 16:23:16 +0900

    feat: add thumbnail image

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductImage
 
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    fields = ['name', 'description', 'price', 'stock']
+    fields = ['name', 'description', 'price', 'stock', 'thumbnail_image']
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+    fields = ['image']
 
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
