adbc8c3e54ff4df55560d0701b1835bd5e758b4b
yezyvibe <yeji0501@gmail.com>
2022-02-21 20:49:53 +0900

    feat: product detail page css

--- frontend/src/components/products/ProductDetail.tsx
+++ frontend/src/components/products/ProductDetail.tsx
@@ -1,14 +1,15 @@
 import { Viewer } from "@toast-ui/react-editor";
 import axios from "axios";
 import Button from "components/common/Button";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import styled, { ThemeConsumer } from "styled-components";
+import styled from "styled-components";
 import { IProduct } from "types/product";
 import { comma } from "utils/comma";
 
 function ProductDetail() {
   const [productInfo, setProductInfo] = useState<IProduct>();
+  const [quantity, setQuantity] = useState(0);
   const params = useParams();
   const getData = async () => {
     try {
@@ -20,6 +21,9 @@ function ProductDetail() {
       console.log(error);
     }
   };
+
+  const onClickQuntity = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
   useEffect(() => {
     getData();
   }, []);
@@ -34,6 +38,27 @@ function ProductDetail() {
         <ProductInfo>
           <Title>{productInfo?.name}</Title>
           <Price>{productInfo ? comma(productInfo.price) : null}원</Price>
+          <Quantity>
+            <Content> 수량 {quantity} </Content>
+            <Button
+              variant="secondary"
+              width="30px"
+              height="30px"
+              borderRadius="50px"
+              onClick={() => setQuantity(quantity + 1)}
+            >
+              +
+            </Button>
+            <Button
+              variant="secondary"
+              width="30px"
+              height="30px"
+              borderRadius="50px"
+              onClick={() => setQuantity(quantity > 0 ? quantity - 1 : 0)}
+            >
+              -
+            </Button>
+          </Quantity>
           <ButtonWrapper>
             <Button variant="primary" width="200px" height="50px">
               구매하기
@@ -75,8 +100,11 @@ const ProductInfo = styled.div`
   flex-direction: column;
   justify-content: space-between;
   width: 550px;
-  height: 550px;
+  height: 300px;
   padding-left: 40px;
+  /* border-width: 0 0 1px 0;
+  border-style: solid;
+  border-color: #e9e5e5; */
 `;
 const Title = styled.h1`
   font-size: 30px;
@@ -84,10 +112,13 @@ const Title = styled.h1`
   color: #363636;
 `;
 const Price = styled.h3`
-  font-size: 24px;
+  font-size: 22px;
   color: #363636;
 `;
 
+const Content = styled.h3`
+  font-size: 22px;
+`;
 const ButtonWrapper = styled.div`
   display: flex;
 `;
@@ -96,3 +127,5 @@ const Description = styled.div`
   margin-top: 200px;
   width: 70%;
 `;
+
+const Quantity = styled.div``;
