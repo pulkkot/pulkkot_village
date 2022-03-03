a975d9999c198f4b8ac3783006064020d8bbfde3
yezyvibe <yeji0501@gmail.com>
2022-03-03 11:55:08 +0900

    fix: remove values which are not used

--- frontend/src/components/home/MainLayout.tsx
+++ frontend/src/components/home/MainLayout.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from "react";
 import styled from "styled-components";
 import Breakpoints from "styles/breakpoints";
 import media from "styles/media";
-import scrollbar from "styles/scrollbar";
 
 interface AppLayoutProps {
   children: ReactNode;
--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,11 +1,9 @@
 import MainLayout from "components/home/MainLayout";
-import ProductAdd from "components/products/ProductAdd";
 import ProductList from "components/products/ProductList";
 
 function ProductPage() {
   return (
     <MainLayout>
-      {/* <ProductAdd /> */}
       <ProductList />
     </MainLayout>
   );
